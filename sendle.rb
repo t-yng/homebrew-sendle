8d1761c6fb6a50206f90f25461346d341c0b6eb6
t-yng <t.yng.greenmonkey@gmail.com>
2020-06-14 19:04:09 +0900

    add sendle.rb

---
+++ sendle.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sendle < Formula
+  desc "send your pdf files to your kindle devices"
+  homepage "https://github.com/t-yng/sendle"
+  url "https://github.com/t-yng/sendle/archive/v0.2.0.tar.gz"
+  sha256 "495f53bad4027fd13fc6a94234cb9a2b624abf9ea1cd831927cdcb3ea3dee6a6"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test sendle`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
